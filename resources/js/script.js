6598b7610ed09ebe8c3a375ac97a5e5a7c272587
Jason Osmond <jasonosmond2001@gmail.com>
2023-10-21 20:50:09 -0600

    Spawning Numbers

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,48 @@
+var spawnDelay = 2;
+var password = 1234;
+const numberBubbles = [
+    "./resources/images/numbers/1.png",
+    "./resources/images/numbers/2.png",
+    "./resources/images/numbers/3.png",
+    "./resources/images/numbers/4.png",
+    "./resources/images/numbers/5.png",
+    "./resources/images/numbers/6.png",
+    "./resources/images/numbers/7.png",
+    "./resources/images/numbers/8.png",
+    "./resources/images/numbers/9.png",
+    "./resources/images/numbers/0.png"
+];
+const spawnOrder = [7,1,2,5,3,8,4,0,9];
+const spawnContainer = document.getElementById("spawn-container");
+
+
+
+function spawnNumberBubble(number) {
+    const randomX = Math.random() * (spawnContainer.clientWidth - 100); // Adjust 100 as needed
+    const bubble = document.createElement("img");
+    bubble.src = numberBubbles[number];
+    bubble.style.position = "absolute";
+    bubble.style.left = randomX + "px";
+    bubble.style.top = "10px"; // Adjust the Y position as needed
+    bubble.style.width = "50px"; // Set the width as needed
+    bubble.style.height = "50px"; // Set the height as needed
+    spawnContainer.appendChild(bubble);
+}
+
+function spawnNumbersInOrder() {
+    let index = 0;
+    const interval = setInterval(() => {
+        if (index < spawnOrder.length) {
+            spawnNumberBubble(spawnOrder[index]);
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 1000); // Adjust the interval as needed
+}
+
+spawnNumbersInOrder();
+
 const basket = document.getElementById('basket');
         let velocity = 0;
         const acceleration = 1;
